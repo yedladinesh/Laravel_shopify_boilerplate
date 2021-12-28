ef805f73fd9aec3bd3da0285a21e5d7507c5d6dd
dinesh <yadladinesh1@gmail.com>
2021-12-28 23:42:35 +0530

    working on config at auth redirect

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->middleware(['verify.shopify'])->name('home');
+
+// Route::get('/', function () {
+//     return 'welcome';
+// })->middleware(['auth.shopify']);

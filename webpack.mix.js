3ebe7c83931c4492e43820a7887690438c7f6df9
dinesh <yadladinesh1@gmail.com>
2021-12-30 00:27:13 +0530

    added tailwind css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const tailwindcss = require('tailwindcss');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
+        require("tailwindcss"),
     ]);
